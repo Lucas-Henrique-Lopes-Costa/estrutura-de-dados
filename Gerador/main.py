14f5de7cea4918d6faa55062b38e6fe35665ff02
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2024-03-15 01:10:10 -0300

    adicionando gerador

--- Gerador/main.py
+++ Gerador/main.py
@@ -1,32 +1,29 @@
-from bs4 import BeautifulSoup
 import re
-import os
+from bs4 import BeautifulSoup
 
 
 def generate_cpp_files(html_file_path):
     # Ler o conteúdo do arquivo HTML
-    with open(html_file_path, 'r', encoding='utf-8') as file:
+    with open(html_file_path, "r", encoding="utf-8") as file:
         html_content = file.read()
 
     # Usar BeautifulSoup para analisar o conteúdo HTML
-    soup = BeautifulSoup(html_content, 'html.parser')
+    soup = BeautifulSoup(html_content, "html.parser")
 
     # Encontrar todos os contêineres de questões
     question_containers = soup.find_all("div", class_="problem-try-ct")
 
     for container in question_containers:
         question_name = container.find("h3").text.strip()
-        description_box = container.find(
-            "div", class_="description-display-box")
+        description_box = container.find("div", class_="description-display-box")
         if description_box:
-            question_description = description_box.get_text(
-                separator="\n", strip=True)
+            question_description = description_box.get_text(separator="\n", strip=True)
         else:
             question_description = "Descrição não encontrada."
 
         # Formatar o nome do arquivo com base no nome da questão
-        filename = re.sub(r'[^\w\s]', '', question_name)
-        filename = ' '.join(filename.split()[1:])
+        filename = re.sub(r"[^\w\s]", "", question_name)
+        filename = " ".join(filename.split()[1:])
         filename = filename.lower() + ".cpp"
 
         # Gerar conteúdo para o arquivo .cpp
@@ -45,7 +42,7 @@ int main() {{
 """
 
         # Criar e escrever no arquivo .cpp
-        with open(filename, "w", encoding='utf-8') as cpp_file:
+        with open(filename, "w", encoding="utf-8") as cpp_file:
             cpp_file.write(cpp_content)
         print(f"Arquivo {filename} gerado com sucesso.")
 
