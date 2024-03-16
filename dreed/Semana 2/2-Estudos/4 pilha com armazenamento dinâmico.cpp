9cee852bf4cc609952b4ef9bf46834f1a38a161e
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2024-03-16 13:33:12 -0300

    100%

--- dreed/Semana 2/2-Estudos/4 pilha com armazenamento dinâmico.cpp
+++ dreed/Semana 2/2-Estudos/4 pilha com armazenamento dinâmico.cpp
@@ -30,11 +30,152 @@ e
 f
 10 8
 */
-
 #include <iostream>
 using namespace std;
 
-int main() {
-    // Seu código aqui
+typedef int Dado;
+
+class Noh
+{
+    friend class Pilha;
+
+private:
+    Dado mDado; // poderia ser outro tipo de variável
+    Noh *mPtProx;
+};
+
+class Pilha
+{
+public:
+    // Constrói pilha vazia.
+    Pilha();
+    // Destrutor que desaloca memória.
+    ~Pilha();
+    // Retira e retorna o valor que estiver no mPtTopo da pilha.
+    // Escreve uma mensagem de erro se não for possível desempilhar.
+    Dado Desempilhar(); // retorna o mPtTopo da Pilha
+    // Insere um valor na pilha.
+    void Empilhar(const Dado &d);
+    // Apagar todos os dados da pilha.
+    void LimparTudo();
+    // Informa quantos valores existem na pilha.
+    unsigned Tamanho() { return mTamanho; }
+    // Retorna o valor que está no mPtTopo sem desempilhar.
+    Dado Topo() const;
+    // Informa se a pilha está Vazia.
+    bool Vazia() const;
+
+private:
+    Noh *mPtTopo;
+    unsigned mTamanho;
+};
+
+Pilha::Pilha()
+{
+    mPtTopo = NULL;
+    mTamanho = 0;
+}
+
+Pilha::~Pilha()
+{
+    LimparTudo();
+}
+
+void Pilha::LimparTudo()
+{
+    Noh *nAux = mPtTopo;
+    while (nAux != NULL) // desalocando todos os nós
+    {
+        Noh *nRemover = nAux; // armazena o nó a ser removido
+        nAux = nAux->mPtProx; // avança na lista
+        delete nRemover;      // apaga o nó
+    }
+    mTamanho = 0;
+    mPtTopo = NULL;
+}
+
+bool Pilha::Vazia() const
+{
+    return (mPtTopo == NULL);
+}
+
+Dado Pilha::Desempilhar()
+{
+    if (Vazia())
+    {
+        cerr << "Pilha vazia" << endl;
+    }
+    else
+    {
+        Noh *nAux = mPtTopo;
+        Dado valor = nAux->mDado;   // armazena o dado a ser retornado
+        mPtTopo = mPtTopo->mPtProx; // atualiza o topo
+        delete nAux;                // apaga o ex-mPtTopo
+        --mTamanho;
+        return valor;
+    }
+    return false;
+}
+
+void Pilha::Empilhar(const Dado &d)
+{
+    Noh *novo = new Noh;
+    novo->mDado = d;
+    novo->mPtProx = mPtTopo;
+    mPtTopo = novo;
+    ++mTamanho;
+}
+
+Dado Pilha::Topo() const
+{
+    if (Vazia())
+    {
+        cerr << "Pilha vazia" << endl;
+    }
+    else
+    {
+        return mPtTopo->mDado;
+    }
+    return false;
+}
+
+int main()
+{
+    Pilha pilha;
+    Dado valor;
+    char comando;
+    do
+    {
+        cin >> comando;
+        switch (comando)
+        {
+        case 'i': // inserir
+            cin >> valor;
+            pilha.Empilhar(valor);
+            break;
+        case 'r': // remover
+            cout << pilha.Desempilhar() << endl;
+            break;
+        case 'l': // limpar tudo
+            pilha.LimparTudo();
+            break;
+        case 't': // consultar tamanho
+            cout << pilha.Tamanho() << endl;
+            break;
+        case 'e': // espiar
+            cout << pilha.Topo() << endl;
+            break;
+        case 'f': // finalizar
+            // checado no do-while
+            break;
+        default:
+            cerr << "comando inválido\n";
+        }
+    } while (comando != 'f'); // finalizar execução
+    while (not pilha.Vazia())
+    {
+        cout << pilha.Desempilhar() << ' ';
+    }
+    cout << endl;
     return 0;
 }
