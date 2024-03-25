c1359bdfba91be48150cc2f4ef462e8048f25d93
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2024-03-25 07:44:53 -0300

    codigo

--- dreed/Semana 3 - Fila/3-Preparatório/2 fila com encadeamento lista de tarefas do jogador.cpp
+++ dreed/Semana 3 - Fila/3-Preparatório/2 fila com encadeamento lista de tarefas do jogador.cpp
@@ -34,9 +34,164 @@ Nome: eliminarBando Premio: dezmilDolares Tipo: a tempo: 30000
 */
 
 #include <iostream>
+#include <stdexcept>
+
 using namespace std;
 
-int main() {
-    // Seu código aqui
+struct Dado
+{
+    string nome;
+    string premio;
+    char tipo;
+    int tempo;
+};
+
+// Imprime as informações de um dado qualquer
+void imprimir_dado(const Dado &umDado)
+{
+    cout << "Nome: " << umDado.nome << " Premio: " << umDado.premio << " Tipo: " << umDado.tipo << " Tempo: " << umDado.tempo << endl;
+}
+
+class Noh
+{
+    friend class Fila;
+
+private:
+    Dado mDado; // poderia ser outro tipo de variável
+    Noh *mProx;
+
+public:
+    Noh(Dado v)
+    {
+        mDado = v;
+        mProx = NULL;
+    }
+};
+
+class Fila
+{
+public:
+    // Constrói Fila vazia.
+    Fila();
+    // Destrutor que desaloca memória.
+    ~Fila();
+    // Retira e retorna o elemento que estiver na frente da Fila.
+    // Escreve uma mensagem de erro se não for possível desenfileirar.
+    Dado Desenfileirar();
+    // Insere um elemento na Fila.
+    void Enfileirar(const Dado &d);
+    // Apagar todos os dados da Fila.
+    void LimparTudo();
+    // Retorna o elemento que está na frente da Fila, sem desenfileirar.
+    inline void Primeiro();
+    // Informa se a Fila está Vazia.
+    inline bool Vazia();
+
+private:
+    Noh *mPtPrimeiro;
+    Noh *mPtrUltimo;
+};
+
+Fila::Fila()
+{
+    mPtPrimeiro = NULL;
+    mPtrUltimo = NULL;
+}
+
+Fila::~Fila()
+{
+    delete mPtPrimeiro;
+}
+
+Dado Fila::Desenfileirar()
+{
+    if (this->Vazia())
+        throw runtime_error("Erro: Fila vazia!");
+    Dado temp = mPtPrimeiro->mDado;
+    Noh *novoPrimeiro = mPtPrimeiro->mProx;
+    delete mPtPrimeiro;
+    mPtPrimeiro = novoPrimeiro;
+
+    return temp;
+}
+
+void Fila::Enfileirar(const Dado &d)
+{
+    Noh *novo = new Noh(d);
+    if (this->Vazia())
+    {
+        mPtPrimeiro = novo;
+        mPtrUltimo = novo;
+    }
+    else
+    {
+        mPtrUltimo->mProx = novo;
+        mPtrUltimo = novo;
+    }
+}
+
+void Fila::LimparTudo()
+{
+    while (!this->Vazia())
+    {
+        this->Desenfileirar();
+    }
+}
+
+void Fila::Primeiro()
+{
+    if (this->Vazia())
+        throw runtime_error("Erro: Fila vazia!");
+    imprimir_dado(mPtPrimeiro->mDado);
+}
+
+bool Fila::Vazia()
+{
+    return mPtPrimeiro == NULL;
+}
+
+int main()
+{
+    Fila fila;
+    Dado info;
+    char comando;
+    do
+    {
+        try
+        {
+            cin >> comando;
+            switch (comando)
+            {
+            case 'i': // inserir
+                cin >> info.nome >> info.premio >> info.tipo >> info.tempo;
+                fila.Enfileirar(info);
+                break;
+            case 'r': // remover
+                imprimir_dado(fila.Desenfileirar());
+                break;
+            case 'l': // limpar tudo
+                fila.LimparTudo();
+                break;
+            case 'e': // espiar
+                fila.Primeiro();
+                break;
+            case 'f': // finalizar
+                break;
+            default:
+                cerr << "comando inválido\n";
+            }
+        }
+        catch (runtime_error &e)
+        {
+            cout << e.what() << endl;
+        }
+    } while (comando != 'f'); // finalizar execução
+
+    while (!fila.Vazia())
+    {
+        imprimir_dado(fila.Desenfileirar());
+    }
+
+    cout << endl;
     return 0;
 }
