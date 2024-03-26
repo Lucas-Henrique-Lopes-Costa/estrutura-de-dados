530ad50c000de13fe7211d89d54994da59fde8c6
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2024-03-25 23:49:55 -0300

    add

--- Aulas/Semana 4 - Heap/3-Preparatório/2 torneio simples.cpp
+++ Aulas/Semana 4 - Heap/3-Preparatório/2 torneio simples.cpp
@@ -24,10 +24,87 @@ Exemplo de Saída:
 15
 */
 
+/*
+ *  Torneio
+ *
+ *  Realiza torneio simples de numeros inteiros
+ *
+ *  Made by Renato Ramos da Silva in 18/09/23
+ *
+ */
 #include <iostream>
+#include <cstring>
+#include <algorithm>
 using namespace std;
 
-int main() {
-    // Seu código aqui
+const int INVALIDO = -1;
+
+class torneio
+{
+private:
+    int *heap;
+    int tamanho;
+    int capacidade;
+    int inicioDados;
+    inline int pai(int filho);
+    inline int esquerda(int getPai);
+    inline int direita(int getPai);
+    void arruma();
+    void compete(int i);
+    void verBaseMaior();
+
+public:
+    torneio(int vet[], int tam);
+    ~torneio();
+};
+
+// Construtor da classe
+torneio::torneio(int vet[], int tam)
+{
+}
+
+// Destrutor da classe
+torneio::~torneio()
+{
+}
+
+// Retorno a posição do nó pai de um determinado elemento
+int torneio::pai(int filho)
+{
+}
+
+// Retorna o filho à esquerda de um determinado nó
+int torneio::esquerda(int pai)
+{
+}
+
+// Retorna o filho à direita do nó pai
+int torneio::direita(int pai)
+{
+}
+
+// Faz a competição por cada posição vencedora no vetor e imprime o vencedor
+void torneio::arruma()
+{
+}
+
+// Faz a competição entre os elementos
+void torneio::compete(int i)
+{
+}
+
+int main()
+{
+    int tam;
+    cin >> tam;
+
+    int vet[tam];
+    for (int i = 0; i < tam; i++)
+    {
+        cin >> vet[i];
+    }
+
+    torneio participantes(vet, tam);
+
     return 0;
 }
