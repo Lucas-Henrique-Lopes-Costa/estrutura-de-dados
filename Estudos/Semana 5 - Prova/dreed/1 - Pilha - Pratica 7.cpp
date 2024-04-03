8dd16bcfb159c35811b87c69d716727818124104
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2024-04-02 23:45:31 -0300

    91

--- Estudos/Semana 5 - Prova/dreed/1 - Pilha - Pratica 7.cpp
+++ Estudos/Semana 5 - Prova/dreed/1 - Pilha - Pratica 7.cpp
@@ -1,3 +1,40 @@
+/*
+Questão 7: Pilha com armazenamento em vetor - Lista de tarefas para robô doméstico - inserção em pilha cheia
+Implemente uma função de inserção que ao inserir um elemento em pilha cheia, o mesmo remove o elemento mais antigo, imprimindo na tela.
+Você pode (
+e deve!
+) utilizar o código que você desenvolveu na atividade "Pilha com armazenamento em vetor - Lista de tarefas para robô doméstico" como base.
+Entradas:
+Com essa finalidade, você não precisará adicionar mais um comando, somente alterar a função de inserção para tratamento do que foi solicitado.
+Saídas:
+Todas as saídas de comandos já estão implementadas na função principal código entregue, somente falta implementar a chamada para a remoção. Ao terminar a execução do programa, todos os itens da pilha são desempilhados e escritos.
+Exemplo de Entrada e Saída juntas:
+r
+Erro: pilha vazia!
+i sala s 150
+i quarto1 q 45
+i quarto2 q 60
+i quarto3 q 45
+i cozinha c 130
+r
+Nome: cozinha Tipo: c Valor: 130
+i cozinha c 143
+e
+Nome: cozinha Tipo: c Valor: 143
+i varanda v 111
+i banheiro b 33
+Nome: sala Tipo: s Valor: 150
+i lavabo l 12
+Nome: quarto1 Tipo: q Valor: 45
+f
+Nome: lavabo Tipo: l Valor: 12
+Nome: banheiro Tipo: b Valor: 33
+Nome: varanda Tipo: v Valor: 111
+Nome: cozinha Tipo: c Valor: 143
+Nome: quarto3 Tipo: q Valor: 45
+Nome: quarto2 Tipo: q Valor: 60
+*/
+
 #include <iostream>
 
 using namespace std;
--- Estudos/Semana 5 - Prova/dreed/2 - Fila - Pratica 3.cpp
+++ Estudos/Semana 5 - Prova/dreed/2 - Fila - Pratica 3.cpp
@@ -1,9 +1,43 @@
+/*
+O jogador resolveu realizar o maior número de tarefas da lista abaixo de um determinado tempo que é informado (valor inteiro positivo). Para isso ele resolveu fazer primeiro as tarefas que tem menor custo até atingir o valor especificado (mas este não incluso). Implemente uma função que imprima o maior número de tarefas da fila em que a soma das tarefas seja menor que o valor informado. Além disso, elimine as atividades da fila.
+
+Você pode (e deve!) utilizar o código que você desenvolveu na atividade "Fila com encadeamento - Lista de tarefas do jogador" como base.
+
+Com essa finalidade, você deverá adicionar mais um comando, como segue:
+
+t seguido de um inteiro: para contabilizar (e imprimir) as atividades que foram realizadas dentro do valor determinado.
+Saídas:
+
+Todas as saídas de comandos já estão implementadas na função principal código entregue, somente falta implementar a chamada para a remoção. Ao terminar a execução do programa, todos os itens da pilha são desempilhados e escritos.
+
+Exemplo de Entrada e Saída juntas:
+
+r
+Erro: Fila vazia!
+i destruirArmazen jaqueta f 100
+i salvarDonzela hb20 e 300
+i montarObservatorioMonteM2 binoculo d 400
+i eliminarChefao milDolares a 2000
+i eliminarBando dezmilDolares a 30000
+t 800
+Nome: destruirArmazen Premio: jaqueta Tipo: f tempo: 100
+Nome: salvarDonzela Premio: hb20 Tipo: e tempo: 300
+r
+Nome: montarObservatorioMonteM2 Premio: binoculo Tipo: d tempo: 400
+e
+Nome: eliminarChefao Premio: milDolares Tipo: a tempo: 2000
+f
+Nome: eliminarChefao Premio: milDolares Tipo: a tempo: 2000
+Nome: eliminarBando Premio: dezmilDolares Tipo: a tempo: 30000
+*/
+
 #include <iostream>
 #include <stdexcept>
 
 using namespace std;
 
-struct Dado {
+struct Dado
+{
     string nome;
     string premio;
     char tipo;
@@ -11,66 +45,85 @@ struct Dado {
 };
 
 // Imprime as informações de um dado qualquer
-void imprimir_dado(const Dado& umDado) {
+void imprimir_dado(const Dado &umDado)
+{
     cout << "Nome: " << umDado.nome << " ";
     cout << "Premio: " << umDado.premio << " ";
     cout << "Tipo: " << umDado.tipo << " ";
-    cout << "Tempo: " << umDado.tempo << " ";
+    cout << "tempo: " << umDado.tempo << " ";
     cout << endl;
 }
 
-class Noh {
+class Noh
+{
     friend class Fila;
-    private:
-        Dado mDado; 
-        Noh* mProx;
-    public:
-        Noh(Dado v) {
-            mDado = v;
-            mProx = NULL;
-        }        
+
+private:
+    Dado mDado;
+    Noh *mProx;
+
+public:
+    Noh(Dado v)
+    {
+        mDado = v;
+        mProx = NULL;
+    }
 };
 
-class Fila {
-    public:
-        // Constrói Fila vazia.
-        Fila();
-        // Destrutor que desaloca memória.
-        ~Fila();
-        // Retira e retorna o elemento que estiver na frente da Fila.
-        // Escreve uma mensagem de erro se não for possível desenfileirar.
-        Dado Desenfileirar(); 
-        // Insere um elemento na Fila.
-        void Enfileirar(const Dado& d);
-        // Apagar todos os dados da Fila.
-        void LimparTudo();
-        // Informa se a Fila está Vazia.
-        inline bool Vazia();
-        // Realiza as tarefas de acordo com o tempo
-        void realizarTarefas(int tempo);
-        // Retorna o elemento que está na frente da Fila, sem desenfileirar.    
-        inline Dado Primeiro(); 
-    private:
-        Noh* mPtPrimeiro;
-        Noh* mPtrUltimo;
-        int tamanho;
+class Fila
+{
+public:
+    // Constrói Fila vazia.
+    Fila();
+
+    // Destrutor que desaloca memória.
+    ~Fila();
+    // Retira e retorna o elemento que estiver na frente da Fila.
+
+    // Escreve uma mensagem de erro se não for possível desenfileirar.
+    Dado Desenfileirar();
+
+    // Insere um elemento na Fila.
+    void Enfileirar(const Dado &d);
+
+    // Apagar todos os dados da Fila.
+    void LimparTudo();
+
+    // Informa se a Fila está Vazia.
+    inline bool Vazia();
+
+    // Realiza as tarefas de acordo com o tempo
+    void realizarTarefas(int tempo);
+
+    // Retorna o elemento que está na frente da Fila, sem desenfileirar.
+    inline Dado Primeiro();
+
+private:
+    Noh *mPtPrimeiro;
+    Noh *mPtrUltimo;
+    int tamanho;
 };
 
-Fila::Fila() { 
+Fila::Fila()
+{
     mPtPrimeiro = mPtrUltimo = NULL;
-    tamanho=0;  
+    tamanho = 0;
 }
 
-Fila::~Fila() {
+Fila::~Fila()
+{
     LimparTudo();
 }
 
 // Retira e retorna o elemento que estiver na frente da Fila.
 // Escreve uma mensagem de erro se não for possível desenfileirar.
-Dado Fila::Desenfileirar() {
-    if (this->Vazia()) throw runtime_error("Erro: Fila vazia!");
-    else{
-        Noh* aux = mPtPrimeiro;
+Dado Fila::Desenfileirar()
+{
+    if (this->Vazia())
+        throw runtime_error("Erro: Fila vazia!");
+    else
+    {
+        Noh *aux = mPtPrimeiro;
         Dado valor = mPtPrimeiro->mDado;
         mPtPrimeiro = mPtPrimeiro->mProx;
         delete aux;
@@ -80,109 +133,132 @@ Dado Fila::Desenfileirar() {
 }
 
 // Insere um elemento na Fila.
-void Fila::Enfileirar(const Dado& d) {
-   Noh* novo = new Noh(d);
-   if (mPtPrimeiro == NULL){
-    mPtPrimeiro = novo;
-   }
-   else{
-    mPtrUltimo->mProx = novo;
-   }
-   mPtrUltimo = novo;
-   tamanho++;      
+void Fila::Enfileirar(const Dado &d)
+{
+    Noh *novo = new Noh(d);
+    if (mPtPrimeiro == NULL)
+    {
+        mPtPrimeiro = novo;
+    }
+    else
+    {
+        mPtrUltimo->mProx = novo;
+    }
+    mPtrUltimo = novo;
+    tamanho++;
 }
 
 // Apagar todos os dados da Fila.
-void Fila::LimparTudo() {
-    while (!Vazia()){
+void Fila::LimparTudo()
+{
+    while (!Vazia())
+    {
         Desenfileirar();
     }
 }
 
 // Informa se a Fila está Vazia.
-bool Fila::Vazia() {
+bool Fila::Vazia()
+{
     return tamanho == 0;
 }
 
 // Retorna o elemento que está na frente da Fila, sem desenfileirar.
-Dado Fila::Primeiro() {
-    if (this->Vazia()) throw runtime_error("Erro: Fila vazia!");
+Dado Fila::Primeiro()
+{
+    if (this->Vazia())
+        throw runtime_error("Erro: Fila vazia!");
     return mPtPrimeiro->mDado;
 }
 
 // Realiza as tarefas de acordo com o menor tempo até o tempo limite
-void Fila::realizarTarefas(int tempo){
-    if (this->Vazia()) throw runtime_error("Erro: Fila vazia!");
+void Fila::realizarTarefas(int tempo)
+{
+    if (this->Vazia())
+        throw runtime_error("Erro: Fila vazia!");
     int soma = 0;
     int tamAux = tamanho;
     int menorTempo;
     Dado aux;
-    while (soma < tempo and !Vazia()){
-        menorTempo = Primeiro().tempo;
+    while (soma < tempo && !Vazia())
+    {
+        menorTempo = Primeiro().tempo; // Inicializa com o primeiro tempo
         // Encontrar o menor tempo
-        for (int i = 0; i < tamAux; i++){
-            aux = Desenfileirar();
-            Enfileirar(aux);
-            if (aux.tempo < menorTempo){
+        for (int i = 0; i < tamAux; i++)
+        {
+            aux = Desenfileirar(); // Desenfileira
+            Enfileirar(aux);       // Enfileira novamente
+            if (aux.tempo < menorTempo)
+            {
                 menorTempo = aux.tempo;
             }
         }
         soma += menorTempo;
         // Verifica se é possivel realizar a tarefa
-        if (soma < tempo){
+        if (soma < tempo)
+        {
             // Procura a tarefa para retirar
-            for (int i = 0; i < tamAux; i++){
+            for (int i = 0; i < tamAux; i++)
+            {
                 aux = Desenfileirar();
-                if (aux.tempo == menorTempo){
+                if (aux.tempo == menorTempo)
+                {
                     imprimir_dado(aux);
                 }
-                else{
+                else
+                {
                     Enfileirar(aux);
                 }
             }
             tamAux = tamanho;
         }
-    }    
+    }
 }
 
-
-int main() {
+int main()
+{
     Fila fila;
     Dado info;
     char comando;
-    do {
-        try{
+    do
+    {
+        try
+        {
             cin >> comando;
-            switch (comando) {
-                case 'i': // inserir
-                    cin >> info.nome >> info.premio >> info.tipo >> info.tempo;
-                    fila.Enfileirar(info);
-                    break;
-                case 'r': // remover
-                    imprimir_dado(fila.Desenfileirar());
-                    break;
-                case 'l': // limpar tudo
-                    fila.LimparTudo();
-                    break;
-                case 'e': // espiar                
-                    imprimir_dado(fila.Primeiro());
-                    break;
-                case 't': // realizar tarefas
-                    int tempo; 
-                    cin >> tempo;
-                    fila.realizarTarefas(tempo);
-                    break;
-                case 'f': // finalizar
-                    // checado no do-while
-                    break;
-                default:
-                    cerr << "comando inválido\n";
+            switch (comando)
+            {
+            case 'i': // inserir
+                cin >> info.nome >> info.premio >> info.tipo >> info.tempo;
+                fila.Enfileirar(info);
+                break;
+            case 'r': // remover
+                imprimir_dado(fila.Desenfileirar());
+                break;
+            case 'l': // limpar tudo
+                fila.LimparTudo();
+                break;
+            case 'e': // espiar
+                imprimir_dado(fila.Primeiro());
+                break;
+            case 't': // realizar tarefas
+                int tempo;
+                cin >> tempo;
+                fila.realizarTarefas(tempo);
+                break;
+            case 'f': // finalizar
+                // checado no do-while
+                break;
+            default:
+                cerr << "comando inválido\n";
             }
-        } catch (runtime_error& e) {
+        }
+        catch (runtime_error &e)
+        {
             cout << e.what() << endl;
         }
     } while (comando != 'f'); // finalizar execução
-    while (not fila.Vazia()) {
+    while (!fila.Vazia())
+    {
         imprimir_dado(fila.Desenfileirar());
     }
     cout << endl;
